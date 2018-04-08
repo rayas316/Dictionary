584e3a4fb69a52116a8b5aac9ffdd7d47bde11e6
Ray Sakakibara <raysakakibara@RaySakakibara-MacBook-Pro.local>
2018-04-08 14:50:57 +0900

    辞書改良

--- app/src/main/java/com/example/raysakakibara/dictionary/MainActivity.java
+++ app/src/main/java/com/example/raysakakibara/dictionary/MainActivity.java
@@ -3,11 +3,12 @@ package com.example.raysakakibara.dictionary;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.Toast;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.TreeSet;
 
@@ -20,7 +21,7 @@ public class MainActivity extends AppCompatActivity {
     HashMap<String, String> hashMap;
     SharedPreferences pref;
     SharedPreferences.Editor editor;
-    TreeSet<String> wordset;
+    TreeSet<String> WordSet;
 
 
     @Override
@@ -33,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
         searchWordEditText = (EditText) findViewById(R.id.searchWord);
         adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
         this.hashMap = new HashMap<>();
-        wordset = new TreeSet<String>();
+        WordSet = new TreeSet<String>();
         pref = getSharedPreferences("dictionary", MODE_PRIVATE);
         editor = pref.edit();
 
@@ -41,15 +42,43 @@ public class MainActivity extends AppCompatActivity {
         editor.putString("develop", "開発する");
         editor.commit();
 
-        wordset.add("technology");
-        wordset.add("develop");
+        WordSet.add("technology");
+        WordSet.add("develop");
 
-        for (String word : wordset) {
+        for (String word : WordSet) {
             this.hashMap.put(word, pref.getString(word, null));
 
             adapter.add("【" + word + "】" + pref.getString(word, null));
         }
         listView.setAdapter(adapter);
 
+    }
+
+    public void add(View v) {
+        String entryWord = wordEditText.getText().toString();
+        String entryMean = meanEditText.getText().toString();
+        String entryObject = "【" + entryWord + "】" + entryMean;
+
+        WordSet.add(entryWord);
+        editor.putString(entryWord, entryMean);
+        editor.putStringSet("WordSet", WordSet);
+        editor.commit();
+        adapter.add(entryObject);
+
+        wordEditText.setText("");
+        meanEditText.setText("");
+    }
+
+    public void search(View v) {
+        String searchWord = searchWordEditText.getText().toString();
+        WordSet.addAll(pref.getStringSet("WordSet", WordSet));
+        for (String word : WordSet) {
+            hashMap.put(word, pref.getString(word, null));
+        }
+        if (hashMap.containsKey(searchWord)) {
+            Toast.makeText(this, hashMap.get(searchWord), Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "この単語は登録されていません", Toast.LENGTH_SHORT).show();
+        }
     }
 }
